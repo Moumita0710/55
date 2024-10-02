54767a1066517008ee57efebd80f7132697b0b42
Moumita0710 <nmoumitaprasad@gmail.com>
2024-10-02 19:06:02 +0530

    Update Emp_Operation.java

--- Emp_Operation.java
+++ Emp_Operation.java
@@ -1,4 +1,5 @@
-package Advance_Java;
+package Oct_2024;
+
 import java.util.*;
 
 public class Emp_Operation {
@@ -6,35 +7,56 @@ public class Emp_Operation {
         Scanner sc = new Scanner(System.in);
         List<Employee> emp = new ArrayList<Employee>();
         String exit;
+        String name;
+        int emp_no,salary;
+        while(true){
+            //
             System.out.println("Select Operation:Insert \n Delete \n Update \n Search \n Display");
             int choice= sc.nextInt();
-           // Iterator<Employee>itr=emp.iterator();itr.hasNext();
-          //  Employee m=itr.next();
-            switch(choice){
+            switch (choice) {
                 case 1:
                     do {
                         System.out.print("Enter Employee name:");
-                        String name = sc.next();
+                        name = sc.next();
                         System.out.print("Enter Employee no:");
-                        int emp_no = sc.nextInt();
+                        emp_no = sc.nextInt();
                         System.out.print("Enter Employee salary:");
-                        int salary = sc.nextInt();
+                        salary = sc.nextInt();
                         System.out.print("Do you want to continue?");
                         exit = sc.next();
-                        Employee employee = new Employee(name, emp_no, salary);
-                        emp.add(employee);
+                        emp.add(new Employee(name, emp_no, salary));
+
                     }
                     while (exit.equalsIgnoreCase("yes"));
                     break;
                 case 2:
+                    System.out.println("*******Display Operation*******");
+                    System.out.println("---------Employee Details---------");
+                    for (
+                            Employee e : emp) {
+                        System.out.println("Employee Name:" + e.getEmp_name());
+                        System.out.println("Employee No:" + e.getEmp_no());
+                        System.out.println("Employee Salary:" + e.getSalary());
+
+                    }
+                    break;
+            case 3:
+                //delete
+                System.out.println("Enter emp no you want ot delete");
+                int del=sc.nextInt();
+                Iterator<Employee>itr=emp.iterator();
+                while(itr.hasNext()){
+                    Employee m=itr.next();
+                    if(m.getEmp_no()==del){
+                        emp.remove(m);
+                        break;
+                    }
+                }
+                break;
+
+//                boolean found=false;
 
             }
-
-        System.out.println("*******Display Operation*******");
-        System.out.println("---------Employee Details---------");
-        for (
-                Employee e : emp) {
-            e.display();
+            }
         }
     }
-}
