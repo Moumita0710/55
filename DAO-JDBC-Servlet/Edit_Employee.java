1c176e4558b51324aa31aacab2c0cfba64b903c8
Moumita0710 <nmoumitaprasad@gmail.com>
2025-01-23 18:51:55 +0530

    Add files via upload

--- DAO-JDBC-Servlet/Edit_Employee.java
+++ DAO-JDBC-Servlet/Edit_Employee.java
@@ -2,11 +2,6 @@ package myPack;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,48 +10,44 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Edit_Employee
+ * Servlet implementation class Edit_Emp
  */
-@WebServlet("/Edit_Employee")
+@WebServlet("/Edit_Emp")
 public class Edit_Employee extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-    
+	
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DAOEmp dao;
+
 		response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 		String id=request.getParameter("id");
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("after get");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/employee","root","");
-			Statement st=con.createStatement();
-            PreparedStatement pstmt = con.prepareStatement("SELECT * FROM emp WHERE ID = ?");
-			 pstmt.setString(1, id);
-	            ResultSet rs = pstmt.executeQuery();
-
-	            if (rs.next()) {
+		dao=new Emp_Dao_Imp();
+		EmpPojo emp=new EmpPojo();
+		
+			try {
+				emp=dao.getId(id);
+			} catch (Exception e) {
+	            e.printStackTrace();
+			}
+	           
 	                out.println("<!DOCTYPE html>");
 	                out.println("<html>");
 	                out.println("<head><title>Edit Employee</title></head>");
 	                out.println("<body>");
 	                out.println("<h1>Edit Employee</h1>");
-	                out.println("<form action='UpdateEmployee' method='post'>");
-	                out.println("<input type='hidden' name='id' value='" + id + "'>");
-	                out.println("Name: <input type='text' name='name' value='" + rs.getString("name") + "' required><br>");
-	                out.println("Password: <input type='password' name='pass' value='" + rs.getString("password") + "' required><br>");
-	                out.println("Email: <input type='email' name='email' value='" + rs.getString("email") + "' required><br>");
-	                out.println("Country: <input type='text' name='country' value='" + rs.getString("country") + "' required><br><br>");
+	                out.println("<form action='Update_Employee' method='post'>");
+	                out.println("<input type='hidden' name='id' value='" + emp.getId() + "'>");
+	                out.println("Name: <input type='text' name='name' value='" + emp.getName()+ "' required><br>");
+	                out.println("Password: <input type='password' name='pass' value='" + emp.getPassword() + "' required><br>");
+	                out.println("Email: <input type='email' name='email' value='" + emp.getEmail() + "' required><br>");
+	                out.println("Country: <input type='text' name='country' value='" + emp.getCountry() + "' required><br><br>");
 	                out.println("<input type='submit' value='Update Employee'>");
 	                out.println("</form>");
 	                out.println("</body>");
 	                out.println("</html>");
-			
-		  }
-		}catch (Exception e) {
-            e.printStackTrace(out);
-        }
-	}
-
+			}
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
